bb3312ce12592967a6ddd6850d13308bf6fe6532
Jayden <jaylecorps@gmail.com>
2024-09-12 11:39:51 -0400

    Adding files

---
+++ shapes.c
@@ -0,0 +1,65 @@
+# include <stdio.h>
+
+
+/*
+Name: Jayden LeCorps
+Date: 9/12/2024
+*/
+
+
+// createPyramid function
+
+void createPyramid(int size){
+
+	for(int i =0; i< size;i++){
+
+		// print blank spaces to move the star toward the center of the pyramid
+		for(int k = 0;k < size - i;k++){
+			printf(" ");
+		}
+
+		// print stars to fill the pyramid
+		for(int j = 0;j <= i*2;j++){
+			printf("*");
+		}
+
+		// new line after each row of stars
+
+		printf("\n");
+
+	}
+}
+
+void createPyramidReverse(int size){
+
+	for(int i = size-1; i >= 0 ;i--){
+
+		// print blank spaces to move the star toward the center of the pyramid
+		for(int k = 0;k < size - i;k++){
+			printf(" ");
+		}
+
+		// print stars to fill the pyramid
+		for(int j = 0;j <= i*2;j++){
+			printf("*");
+		}
+
+		// new line after each row of stars
+
+		printf("\n");
+
+	}
+}
+
+int main(){
+
+	// call the createPyramid function 
+
+	createPyramid(7);
+
+	printf("\n");
+
+	createPyramidReverse(7);
+
+	return 0;
+}
