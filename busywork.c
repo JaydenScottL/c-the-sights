63a98b0a5e632decb9a5203bf5f40ef3865fde3f
Jayden <jaylecorps@gmail.com>
2024-09-24 12:10:13 -0400

    added comments

--- busywork.c
+++ busywork.c
@@ -2,8 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+// creates a square with user input
+
 void squareCreate(char charString[], int shapeSize, size_t stringSize){
 
+    // if string is empty, print only stars
     if(stringSize == 0){
         charString = "*";
         stringSize = 1;
@@ -15,7 +18,7 @@ void squareCreate(char charString[], int shapeSize, size_t stringSize){
     
     
     for(int i = 0; i < shapeSize;i++){
-
+        // print full row only if first or last run of loop
         if(i == 0 || i == shapeSize -1){
             for(int j = 0; j < shapeSize;j++){
                 printf("%c",charString[index % stringSize]);
@@ -23,6 +26,7 @@ void squareCreate(char charString[], int shapeSize, size_t stringSize){
             }
         }else{
 
+            // print a character, followed by spaces, and another character to create the unfilled square
             printf("%c",charString[index % stringSize]);
 
             index++;
@@ -46,7 +50,7 @@ void squareCreate(char charString[], int shapeSize, size_t stringSize){
 
 void emptyDiamond(char charString[], int shapeSize, size_t stringSize){
 
-
+    // if string is empty, print only stars
     if(stringSize == 0){
         charString = "*";
         stringSize = 1;
@@ -54,18 +58,22 @@ void emptyDiamond(char charString[], int shapeSize, size_t stringSize){
 
     int index = 0;
 
-	for(int i =0; i< shapeSize;i++){
+    // prints first half of diamond
+	for(int i = 0; i< shapeSize;i++){
 
+        // print number of characters equal to the shapesize - current loop index
 		for(int k = 0;k < shapeSize - i ;k++){
 			
             printf("%c",charString[index % stringSize]);
             index++;
 		}
 
+        // fill with whitespaces
 		for(int j = 0;j < i*2 ;j++){
 			printf(" ");
 		}
 
+        // print other half of diamond
         for(int k = 0;k < shapeSize - i ;k++){
 			
             printf("%c",charString[index % stringSize]);
@@ -78,17 +86,20 @@ void emptyDiamond(char charString[], int shapeSize, size_t stringSize){
 
 	}
 
+    // prints lower half of diamond
     for(int i = shapeSize; i >= 0 ;i--){
-
+        // print number of characters equal to the shapesize - current loop index
 		for(int k = 0;k < shapeSize - i;k++){
             printf("%c",charString[index % stringSize]);
             index++;
 		}
 
+        // fill with whitespaces
 		for(int j = 0;j < i*2;j++){
 			printf(" ");
 		}
 
+        // print other half of diamond
         for(int k = 0;k < shapeSize - i;k++){
             printf("%c",charString[index % stringSize]);
             index++;
@@ -101,14 +112,17 @@ void emptyDiamond(char charString[], int shapeSize, size_t stringSize){
 
 int main(int argc, char *argv[]){
 
+    // make sure there are 3 user defined arguments
     if(argc < 4){
         printf("Need at least 3 arguments\n");
         return 0;
     }
 
 
+    // checks that length of shape choice string is > 0, and shape size > 0
     if(strlen(argv[1]) > 0 && atoi(argv[3]) > 0){
 
+        // prints shape based on user input
         if(strcmp(argv[1],"square") == 0){
 
             printf("Creating square with input \"%s\" and size %d\n", argv[1],atoi(argv[2]));
